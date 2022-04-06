7f67a46fa3f4411b31e74146694cb2c3b4471a18
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2022-04-06 12:54:39 +0200

    Improved input and output. Worked on game's logic.
    The game now reacts to *certain* combinations. Further works is needed to make it work.

--- src/App.java
+++ src/App.java
@@ -2,12 +2,28 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("[ - - - The Tris Game - - - ]\n");
 
-        int move, turn;
+        int move = 0;
+        char playerID  = 'X';
         Boolean hasWon = false;
         Board board = new Board();
 
-        for (int j = 0; j < 9; j++) {
-            board.makeMove('X');
+        while (hasWon != true && move < 9) {
+            if (playerID != 'X') {
+                playerID = 'X';
+            } else {
+                playerID = 'O';
+            }
+
+            board.makeMove(playerID);
+            hasWon = board.checkIfWon();
+            
+            move++;
+        }
+
+        if (hasWon == true) {
+            System.out.println("Congratulazioni Giocatore " + playerID + ", hai vinto!");
+        } else {
+            System.out.println("Pareggio! Riavviate il gioco per ricominciare la sfida.");
         }
     }
 }
--- src/Board.java
+++ src/Board.java
@@ -9,7 +9,7 @@ public class Board {
         int index = 1;
         char player = playerID;
 
-        System.out.print("Dove vuoi fare la tua mossa? [1 - 9] ");
+        System.out.print("Giocatore " + player + ", tocca a te! \nDove vuoi fare la tua mossa? [1 - 9] ");
 
         try {
             index = input.nextInt(); 
@@ -18,17 +18,44 @@ public class Board {
             input.nextLine();
         }
 
-        if (index > 0 && index < 10 && cells[index] != 'X' && cells[index] != 'O') {
+        System.out.print("\n");
+
+        if (index > 0 && index < 10 && cells[index - 1] != 'X' && cells[index - 1] != 'O') {
             cells[index - 1] = playerID;
 
-            for (int i = 0; i < cells.length; i++) {
-                System.out.print(cells[i] + " ");
-            }
-
-            System.out.print("\n");
+            this.printBoard();
         } else {
             System.out.println("Questo spazio è già occupato, o hai inserito un numero non permesso. Riprova!\n");
             this.makeMove(player);
         }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < cells.length; i++) {
+
+            if (cells[i] == 0) {
+                System.out.print("[" + " " + "]" + " ");
+            } else {
+                System.out.print("[" + cells[i] + "]" + " ");
+            }
+
+            if (i == 2 || i == 5) {
+                System.out.print("\n");
+            }
+        }
+
+        System.out.print("\n\n");
+    }
+
+    public Boolean checkIfWon() {
+        if (cells[0] == cells[1] && cells[0] == cells[2] && cells[0] != 0) {
+            return true;
+        }
+
+        if (cells[3] == cells[4] && cells[3] == cells[5] && cells[3] != 0) {
+            return true;
+        }
+
+        return false;
     }
 }
